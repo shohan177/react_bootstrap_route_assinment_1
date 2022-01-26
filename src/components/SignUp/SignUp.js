52d2c907d5000d97265061c48d1dfb8571fc9609
Sarwar Jahan Shohan <sarwar.shohan24@gmail.com>
2022-01-26 19:22:24 +0600

    form submit

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -3,6 +3,13 @@ import { Container,Row,Col ,Form, Card, Button } from 'react-bootstrap';
 import './SignUp.css';
 
 const SignUp = () => {
+    let handleSignUpForm = (e) => {
+        e.preventDefault();
+
+        let form_data = new FormData(e.target)
+        console.log(Object.fromEntries(form_data.entries()));
+        
+    }
     return (
         <>
             <Container>
@@ -11,22 +18,22 @@ const SignUp = () => {
                         <Card className='shadow'>
                             <Card.Body>
                                 <h2>Create an Account</h2>
-                                <Form>
-                                    <Form.Group>
+                                <Form onSubmit={handleSignUpForm}>
+                                    <Form.Group >
                                         <Form.Label>Full Name</Form.Label>
-                                        <Form.Control></Form.Control>
+                                        <Form.Control name='name'></Form.Control>
                                     </Form.Group>
                                     <Form.Group>
                                         <Form.Label>Email</Form.Label>
-                                        <Form.Control></Form.Control>
+                                        <Form.Control name='email'></Form.Control>
                                     </Form.Group>
                                     <Form.Group>
                                         <Form.Label>User Name</Form.Label>
-                                        <Form.Control></Form.Control>
+                                        <Form.Control name='user_name'></Form.Control>
                                     </Form.Group>
                                     <Form.Group>
                                         <Form.Label>Subject</Form.Label>
-                                        <Form.Select>
+                                        <Form.Select name='edu'>
                                             <option value=''>--Select--</option>
                                             <option value=''>CSE</option>
                                             <option value=''>EEE</option>
@@ -43,13 +50,13 @@ const SignUp = () => {
                                     </Form.Group>
                                     <Form.Group>
                                         <br/>
-                                        <input type='checkbox' value='agree' id='agree'></input>
+                                        <input type='checkbox' name='agree' value='agree' id='agree'></input>
                                         <label for='agree'>I agree</label>
                                     </Form.Group>
                                     <hr></hr>
                                     <Form.Group>
                                         <br></br>
-                                        <Button variant='primary' type='submit' className='btn-block'>SIGN UP NOW</Button>
+                                        <Button variant='primary' type='submit ' className='btn-block'>SIGN UP NOW</Button>
                                     </Form.Group>
 
                                 </Form>
